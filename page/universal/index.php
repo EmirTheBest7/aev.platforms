7b53f5dcda48281f48df025c073f353286e77675
Emir Aliev <EmirTheBest7@gmail.com>
2024-03-12 22:21:41 +0100

    Universal Page Added
    Design template for other static pages [History, Our Team, Philosphy, etc.]

---
+++ page/universal/index.php
@@ -0,0 +1,345 @@
+<?php 
+
+include('../../_inc/functions.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport"
+		content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover">
+	<link rel="shortcut icon" type="image/x-icon" href="img/EAlogo.svg">
+
+	<title>ΛΞV</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+	<link rel="stylesheet" href="<?php echo BASE_URL . "_assets/css/core.css" ?>">
+	<link rel="stylesheet" href="./style.css">
+
+
+</head>
+
+<body>
+	<nav class="Navbar">
+		<a href="#" id="toggle" class="Toggle Navbar-toggle" data-toggle="collapse"
+			data-target="#navbarCollapse"><span></span></a>
+
+		<img class="Navbar-brand u-pullRight Navbar-brand-mobile" href="#link" src="<?php echo LOGO; ?>">
+
+		<div id="navbarCollapse" class="Navbar-menu">
+
+		</div>
+
+		<ul class="Navbar-quickLinks">
+
+		</ul>
+	</nav>
+
+	<main>
+
+		<header id="header">
+			<div class="contain">
+				<div class="breadcrumb">
+					<img src="../downloads/logo/A.svg" alt="" style="height: 35px; margin: 8px 0px;">
+				</div>
+				<div class="global_links">
+					<i class="uil uil-dice-five"></i>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</header>
+
+		<header class="l-header l-header--extended" style="padding-top: 87.5px;">
+
+			<div class="l-header__background" style="background-color: rgb(25,25,25)">
+
+			</div>
+
+			<div class="l-container">
+				<div class="l-grid h-push-bottom">
+
+					<div class="l-col-12 l-col-sm-4 l-col--hidden-sm-down">
+
+						<ul class="c-meta">
+							<li class="c-meta__item">
+
+								<span class="c-meta__content">
+									<img src="./scc.svg" style="width: 150px; filter: invert(1); position: absolute;">
+								</span>
+
+							</li>
+						</ul>
+
+					</div>
+
+					<div class="l-col-12 l-col-sm-6">
+						<div class="c-header">
+							<h1 class="c-header__title">Our Team</h1>
+						</div>
+					</div>
+				</div>
+
+
+
+				<div class="l-grid">
+					<div class="l-col-12 l-col-sm-6 l-col-sm-offset-4">
+						<div class="l-grid c-meta c-header__featured-meta">
+
+							<div class="l-col-6 l-col-mi-4 l-col-sm-4">
+								<span class="c-meta__item">
+									<span class="c-meta__label">Meta</span>
+
+									<span class="c-meta__content">Center</span>
+
+								</span>
+							</div>
+
+							<div class="l-col-6 l-col-mi-4 l-col-sm-4">
+
+								<span class="c-meta__label">Meta</span>
+
+								<span class="c-meta__content c-meta__term">Center</span>
+							</div>
+
+							<div class="l-col-6 l-col-mi-4 l-col-sm-4">
+
+								<span class="c-meta__label">Meta</span>
+
+								<span class="c-meta__content c-meta__term">Center</span>
+							</div>
+
+
+						</div>
+					</div>
+				</div>
+
+
+			</div>
+		</header>
+
+
+
+		<section class="l-case-media">
+			<div class="l-container">
+				<div class="l-grid">
+
+					<div class="c-media c-frame c-frame--tablet">
+
+						<div class="c-frame__inner">
+							<div class="c-frame__content">
+
+
+							</div>
+						</div>
+					</div> <!-- .c-media -->
+				</div>
+			</div>
+		</section>
+
+		<section class="l-case-content">
+			<div class="l-container">
+
+				<div class="c-card js-scrollspy a-fadeInUp was-in-view is-in-view">
+					<div class="l-grid">
+
+						<div class="l-col-12 l-col-sm-4">
+							<h2 class="c-card__heading">Heading</h2>
+						</div>
+
+						<div class="l-col-12 l-col-sm-6 l-col-sm-offset-2">
+
+							<div class="c-card__content">
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin augue urna, rutrum id
+									lacus eu, facilisis interdum nunc. Sed varius cursus sagittis. Etiam sit amet nunc
+									vel lectus pulvinar scelerisque. Pellentesque ac dictum tellus. Ut lacus odio,
+									molestie ac velit ac, iaculis volutpat odio. In gravida consequat quam vel volutpat.
+									Curabitur volutpat facilisis leo a ornare. Mauris mattis libero vitae venenatis
+									scelerisque. Aenean ornare viverra molestie.<br><br>
+
+									Integer laoreet fringilla tellus eu bibendum. Cras fermentum dolor quis nibh
+									lacinia, in ullamcorper lacus vulputate. Fusce eros mauris, posuere et vulputate et,
+									tincidunt sed nunc. Nulla venenatis mi a consectetur tempor. Duis elementum, lacus
+									quis pretium malesuada, nisi urna faucibus leo, non fringilla felis tellus sed nisl.
+									Nunc in lobortis eros. Phasellus pharetra suscipit finibus. Cras condimentum, nibh
+									placerat iaculis hendrerit, turpis risus sodales dolor, non egestas lacus turpis ut
+									mauris. Mauris condimentum nunc turpis, id feugiat arcu lobortis et. Nunc vitae
+									convallis neque. In hac habitasse platea dictumst. Phasellus eros arcu, convallis id
+									pellentesque et, cursus id metus. Vivamus eu ultrices urna. Aenean arcu turpis,
+									varius at est sed, tempus luctus velit. Nulla nulla magna, pretium ut purus id,
+									sodales placerat neque.
+								</p>
+							</div>
+
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</section>
+
+		<section class="l-section">
+			<div class="l-container">
+				<div class="l-grid">
+					<div class="l-col-12">
+						<a class="c-card c-card--image" href="#">
+							<div class="l-grid l-grid--reverse-md-down">
+
+								<div class="l-col-12 l-col-sm-6 l-col--equal-height">
+									<h2 class="c-card__heading">Next Project</h2>
+
+									<div class="c-card__content">
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam est lorem,
+										iaculis nec tortor at, bibendum rutrum arcu. In eleifend libero orci, sed
+										ullamcorper justo dapibus tristique.
+									</div>
+
+									<div class="c-card__footer c-meta">
+
+										<span class="c-meta__content c-meta__term">Page</span><span
+											class="c-meta__content c-meta__term">Index</span>
+									</div>
+								</div>
+
+								<div class="l-col-12 l-col-sm-6 l-col--equal-height">
+									<div class="c-card__media">
+
+										<img width="992" height="595" src="#" style="display: none;"
+											class="c-card__image">
+
+										<span class="c-featured-btn">Next <img class="icon" src="./scc.svg">
+										</span>
+									</div>
+								</div>
+
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<footer id="site-footer">
+
+			<section class="horizontal-footer-section" id="footer-top-section">
+				<div id="footer-logo">
+					<picture>
+						<img src="<?php echo LOGO;?>" alt="footer logo" role="presentation">
+					</picture>
+				</div>
+				<div id="footer-top-menu-container" role="menubar">
+					<ul id="footer-top-menu" role="menu">
+						<li class="footer-top-menu-item" role="menuitem">
+							<a href="#" class="footer-top-menu-link">IT Services</a>
+						</li>
+						<li class="footer-top-menu-item footer-top-menu-item-active" role="menuitem">
+							<a href="#" class="footer-top-menu-link">Web development</a>
+						</li>
+						<li class="footer-top-menu-item" role="menuitem">
+							<a href="#" class="footer-top-menu-link">SEO</a>
+						</li>
+						<li class="footer-top-menu-item" role="menuitem">
+							<a href="#" class="footer-top-menu-link">UI/UX</a>
+						</li>
+					</ul>
+				</div>
+				<div id="footer-buttons-container">
+					<a href="#" class="ripple-button" role="button">View our Projects</a>
+				</div>
+			</section>
+
+			<section class="horizontal-footer-section" id="footer-middle-section">
+				<div id="footer-about" class="footer-columns footer-columns-large">
+					<h1>Our Address</h1>
+					<address>
+						<p><i class="uil uil-map-marker"></i>ХЗ</p>
+						<p><i class="uil uil-phone-alt"></i>(+420) 123 456 789</p>
+						<p><i class="uil uil-envelope-alt"></i></img>hello@aliev.io</p>
+						<p><i class="uil uil-clock-eight"></i>8:00 AM – 8:00 PM</p>
+					</address>
+				</div>
+				<div class="footer-columns">
+					<h1>Overview</h1>
+					<ul class="footer-column-menu" role="menu">
+						<li class="footer-column-menu-item" role="menuitem">
+							<a href="#" class="footer-column-menu-item-link">Services </a>
+						</li>
+						<li class="footer-column-menu-item" role="menuitem">
+							<a href="#" class="footer-column-menu-item-link">Pricing</a>
+						</li>
+						<li class="footer-column-menu-item" role="menuitem">
+							<a href="#" class="footer-column-menu-item-link">Portfolio</a>
+						</li>
+						<li class="footer-column-menu-item" role="menuitem">
+							<a href="#" class="footer-column-menu-item-link">News</a>
+						</li>
+					</ul>
+				</div>
+				<div class="footer-columns">
+					<h1>Resources</h1>
+					<ul class="footer-column-menu" role="menu">
+						<li class="footer-column-menu-item" role="menuitem">
+							<a href="#" class="footer-column-menu-item-link">FAQ</a>
+						</li>
+						<li class="footer-column-menu-item" role="menuitem">
+							<a href="#" class="footer-column-menu-item-link">Media</a>
+						</li>
+						<li class="footer-column-menu-item" role="menuitem">
+							<a href="#" class="footer-column-menu-item-link">Guides</a>
+						</li>
+						<li class="footer-column-menu-item" role="menuitem">
+							<a href="#" class="footer-column-menu-item-link">Free Resources</a>
+						</li>
+						<li class="footer-column-menu-item" role="menuitem">
+							<a href="#" class="footer-column-menu-item-link">Testimonials</a>
+						</li>
+					</ul>
+				</div>
+				<div class="footer-columns">
+					<h1>Information</h1>
+					<ul class="footer-column-menu" role="menu">
+						<li class="footer-column-menu-item" role="menuitem">
+							<a href="#" class="footer-column-menu-item-link">About Us</a>
+						</li>
+						<li class="footer-column-menu-item" role="menuitem">
+							<a href="#" class="footer-column-menu-item-link">Terms of Use</a>
+						</li>
+						<li class="footer-column-menu-item">
+							<a href="#" class="footer-column-menu-item-link" role="menuitem">Legal Information</a>
+						</li>
+						<li class="footer-column-menu-item" role="menuitem">
+							<a href="#" class="footer-column-menu-item-link">Message Us</a>
+						</li>
+						<li class="footer-column-menu-item" role="menuitem">
+							<a href="#" class="footer-column-menu-item-link">Leave a Feedback</a>
+						</li>
+					</ul>
+				</div>
+			</section>
+
+			<section class="horizontal-footer-section" id="footer-bottom-section">
+				<div id="footer-copyright-info">
+					&copy; Designed by <img style="height:11px;" src="<?php echo LOGO; ?>">
+					Platforms. <script>
+						document.write(new Date().getFullYear())
+					</script>. All rights reserved.
+				</div>
+				<div id="footer-social-buttons">
+					<i class="uil uil-instagram"></i>
+				</div>
+			</section>
+
+		</footer>
+
+
+	</main>
+	<!-- partial -->
+	<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
+	<script src='https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js'></script>
+
+
+</body>
+
+</html>
