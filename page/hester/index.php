0037787404379992334114d94d4219fc79b56ee1
Emir Aliev <EmirTheBest7@gmail.com>
2024-08-29 03:34:33 +0200

    Update index.php

--- page/hester/index.php
+++ page/hester/index.php
@@ -9,6 +9,8 @@ session_start();
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport"
+    content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover">
   <title>HesterGPT Chatbot</title>
   <link rel="stylesheet" href="./style.css">
 
