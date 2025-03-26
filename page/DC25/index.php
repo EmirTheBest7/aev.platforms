749fb7d8799e187d83e8073a1e41e1404ff0d3f9
Emir Aliev <EmirTheBest7@gmail.com>
2025-03-26 21:38:46 +0100

    ΛWDC25 Page Added
    + Page invitation Added
    + Created RSVP
    + Created Marketing posters

---
+++ page/DC25/index.php
@@ -0,0 +1,56 @@
+<?php 
+
+include('../../_inc/functions.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>ΛΞV | ΛWDC25</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
+  <link rel="stylesheet" href="<?php echo BASE_URL . "_assets/css/core.css" ?>">
+  <link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<nav class="Navbar">
+		<a href="#" id="toggle" class="Toggle Navbar-toggle" data-toggle="collapse"
+			data-target="#navbarCollapse"><span></span></a>
+
+		<img class="Navbar-brand u-pullRight Navbar-brand-mobile" href="#link" src="<?php echo LOGO; ?>">
+
+		<div id="navbarCollapse" class="Navbar-menu">
+
+		</div>
+
+		<ul class="Navbar-quickLinks">
+
+		</ul>
+  </nav>
+  
+<div class="container">
+  <div class="wrapper">
+    <div class="content">
+      
+    <div class="title">
+      <h3>Get ready for</h3>
+      <h1>ΛWDC25</h1>
+      <h3>Prague, CZ <strong>MAY 30-31</strong></h3>
+      <h4>C O N C E P T</h4>
+    </div>
+
+    </div>
+  </div>
+</div>
+<!-- partial -->
+
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/pixi.js/4.4.2/pixi.min.js'></script>
+<script  src="./script.js"></script>
+  
+</body>
+</html>
